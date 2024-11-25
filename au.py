c3a48c41b27510c1c4ae4f3b53172087efbbba42
shakalov111 <alex070shakalov@gmail.com>
2024-11-25 22:14:15 +0200

    add writer finction

--- au.py
+++ au.py
@@ -3,50 +3,40 @@ import os
 import random
 import getpass
 import pygetwindow as gw
-import sqlite3
+
 
 
 password = "0"
 question = "Сколько:"
 
-def createdb():
-    conn = sqlite3.connect("question.db")
-    cursor = conn.cursor()
-    create_table = '''
-    CREATE TABLE IF NOT EXISTS user_action(
-    id INTEGER PRYMARY KEY,
-    name TEXT NOT NULL,
-    time TEXT)
-    '''
-    conn.execute(create_table)
-    conn.commit()
-    conn.close()
+def util():
+    window = gw.getActiveWindowTitle()
+    windows = window.split("-")
+    with open('users_action.data',"w") as file:
+                file.write('Дії користувача:')
+    for i in windows:
+        massage = f'''
+        {i}
+        {time.strftime("%Y,%m,%d,%H:%M,%S",time.localtime())}
+        '''
+        try:
+            with open('users_action.data',"a") as file:
+                file.write(massage)
+        except:
+            print("Error")
 
-def insert(str):
-    time0 = time.strftime("%Y,%m,%d,%H:%M,%S",time.localtime())
-    add_new_action = f'''
-    INSERT INTO user_action(name,time) VALUES ({str},{time0})
-    '''
-def select():
-    conn = sqlite3.connect("question.db")
-    cursor = conn.cursor()
-    select_query = 'Select * FROM user_action'
-    cursor.execute(select_query)
-    data = cursor.fetchall()
-    print(data)
 def main ():
     while True:
         window = gw.getActiveWindowTitle()
-
+        util()
         answer = getpass.getpass(question)
         if answer == password:
             print("Вірно")
         else:
             #os.system("shutdown /l")
-            print('os.system("shutdown /l")')
+            print('os.system("shutdown /l /t 10")')
 
 if __name__ == "__main__":
-    createdb()
-    insert(gw.getActiveWindowTitle())
-    select()
+
     main()
+    
