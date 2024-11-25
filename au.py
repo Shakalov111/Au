94cb2347504529472f94f271f09ae55e4a95c745
shakalov111 <alex070shakalov@gmail.com>
2024-11-25 22:58:48 +0200

    додано запис при неправильній відповіді

--- au.py
+++ au.py
@@ -6,13 +6,13 @@ import pygetwindow as gw
 
 
 
-password = "0"
-question = "Сколько:"
 
-def util():
+questions = {"Питання 1":'1',"Питання 2":'2',"Питання 3":'3',"Питання 4":'4',"Питання 5":'5',"Питання 6":'6'}
+N = 1
+def track_active_window():
     window = gw.getActiveWindowTitle()
     windows = window.split("-")
-    with open('users_action.data',"w") as file:
+    with open('users_action.data',"a") as file:
                 file.write('Дії користувача:')
     for i in windows:
         massage = f'''
@@ -25,16 +25,25 @@ def util():
         except:
             print("Error")
 
+def t():
+       try:
+            with open('users_action.data',"a") as file:
+                file.write("Введено невірний пароль\n\t"+ time.strftime("%Y,%m,%d,%H:%M,%S",time.localtime()))
+       except:
+            print("Error")
 def main ():
     while True:
-        window = gw.getActiveWindowTitle()
-        util()
+        track_active_window()
+        question = list(questions.keys())[random.randint(0,5)]
         answer = getpass.getpass(question)
-        if answer == password:
+        if answer == questions[question]:
             print("Вірно")
         else:
             #os.system("shutdown /l")
             print('os.system("shutdown /l /t 10")')
+            t()
+
+        time.sleep(N*60)
 
 if __name__ == "__main__":
 
