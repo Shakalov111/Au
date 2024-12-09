cb456be20caaf4a838136cc22144c59f909f03ca
shakalov111 <alex070shakalov@gmail.com>
2024-12-09 22:44:07 +0200

    Додано запис невдалої спроби

--- au.py
+++ au.py
@@ -9,11 +9,13 @@ import pygetwindow as gw
 
 questions = {"Питання 1":'1',"Питання 2":'2',"Питання 3":'3',"Питання 4":'4',"Питання 5":'5',"Питання 6":'6'}
 N = 1
-def track_active_window():
+def track_active_window(flag:bool):
     window = gw.getActiveWindowTitle()
     windows = window.split("-")
     with open('users_action.data',"a") as file:
-                file.write('Дії користувача:')
+                file.write('Дії користувача:\n\t')
+                if flag:
+                     file.write("Введено невірний пароль\n\t"+ time.strftime("%Y,%m,%d,%H:%M,%S",time.localtime()))
     for i in windows:
         massage = f'''
         {i}
@@ -25,24 +27,18 @@ def track_active_window():
         except:
             print("Error")
 
-def t():
-       try:
-            with open('users_action.data',"a") as file:
-                file.write("Введено невірний пароль\n\t"+ time.strftime("%Y,%m,%d,%H:%M,%S",time.localtime()))
-       except:
-            print("Error")
+
 def main ():
     while True:
-        track_active_window()
         question = list(questions.keys())[random.randint(0,5)]
         answer = getpass.getpass(question)
         if answer == questions[question]:
             print("Вірно")
+            track_active_window(False)
         else:
             #os.system("shutdown /l")
             print('os.system("shutdown /l /t 10")')
-            t()
-
+            track_active_window(True)
         time.sleep(N*60)
 
 if __name__ == "__main__":
